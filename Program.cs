8f6680ce1478379c68961f4ba66bb9d29f6d5808
ShakedBu <buch.shaked@gmail.com>
2020-04-19 18:33:19 +0300

    get articales from page method

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.IO;
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
 using System.Net;
 
 namespace ThreadsHeadlines
@@ -8,31 +9,44 @@ namespace ThreadsHeadlines
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var articles = GetArticlesFromPage("https://www.bbc.com/news/world", "li", "nw-c-nav__secondary-menuitem-container");
+
+            foreach (var curr in articles)
+            {
+                Console.WriteLine(curr.Key + ": " + curr.Value);
+            }
         }
 
-        static void GetArticlesFromPage(string url, string tag, string className)
+        static Dictionary<string, string> GetArticlesFromPage(string url, string tag, string className)
         {
-            // Get the HTML
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            var result = new Dictionary<string, string>();
+            HtmlDocument HtmlDoc = new HtmlDocument();
 
-            // If we got a result - not an error
-            if (response.StatusCode == HttpStatusCode.OK)
+            // Base url
+            var uri = new Uri(url);
+            string baseUrl = uri.Scheme + "://" + uri.Host;
+
+            // Get the HTML from the url
+            var client = new WebClient();
+            var HtmlStr = client.DownloadString(url);
+            HtmlDoc.LoadHtml(HtmlStr);
+
+            // Get the relevant elements
+            HtmlNodeCollection elements = HtmlDoc.DocumentNode.SelectNodes("//" + tag + "[contains(@class, '" + className + "')]");
+
+            // Get the titles and the full links for the articles
+            foreach (HtmlNode curr in elements)
             {
-                Stream receiveStream = response.GetResponseStream();
-                StreamReader readStream = null;
+                string title = curr.InnerText;
+                string link = curr.GetAttributeValue("href", "none") != "none" ? 
+                              curr.GetAttributeValue("href", "none") : 
+                              curr.ChildNodes["a"].GetAttributeValue("href", "none");
+                string fullLink = link.StartsWith("/") ? baseUrl + link : link;
 
-                if (String.IsNullOrWhiteSpace(response.CharacterSet))
-                    readStream = new StreamReader(receiveStream);
-                else
-                    readStream = new StreamReader(receiveStream, System.Text.Encoding.UTF8);
-
-                string HTML = readStream.ReadToEnd();
-
-                response.Close();
-                readStream.Close();
+                result[title] = fullLink;
             }
+
+            return result;
         }
     }
 }
